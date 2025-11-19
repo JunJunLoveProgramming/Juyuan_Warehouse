c0e9e66f4ca0201fa4cb04643df28befe362c606
NEKO-0032 <535902116@qq.com>
2025-11-19 23:08:03 +0800

    先完善一下文件

--- hook-maink.py
+++ hook-maink.py
@@ -1,4 +1,30 @@
 # hook-maink.py
 from PyInstaller.utils.hooks import collect_all
+import PyInstaller.__main__ as pm
 
 datas, binaries, hiddenimports = collect_all('maink')
+
+class pmPackage:
+    def __init__(self):
+        self.tagList = []#在列表里填入键值，自动生成全否定字典
+        self.tagDic = {'--onefile':True,'--windowed':True}
+        self.fileName = None
+
+    def runPachage(self):
+        pm.run(self._getTrueList())
+
+
+    def _getTrueList(self):
+        result = []
+        result.append(self.fileName)
+
+        for i in self.tagDic.keys():
+            if self.tagDic[i]:
+                result.append(i)
+        return  result
+    def setFileName(self,name):
+        self.fileName = name
+
+if __name__ == "__main__":
+    pmK = pmPackage()
+    pmK.tagDic['--windowed'] = False
